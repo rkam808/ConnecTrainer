918486bbf23cce21ccee2f5ffe9a1b3c96adea19
Ryan Kam <rkam0725@gmail.com>
2021-05-25 19:11:51 +0900

    fixed dashboard bookings lists

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
 
   def index
-    # @outgoing_requests = policy_scope(Booking).all
+    @outgoing_requests = policy_scope(current_user.bookings).all
     @my_workouts = policy_scope(current_user.workouts).all
   end
 end
