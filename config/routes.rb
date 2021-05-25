6ae6ee498cc3f3712d2cc20c114142d621adfec9
Ryan Kam <rkam0725@gmail.com>
2021-05-25 16:10:34 +0900

    finished basic dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:new, :create, :show]
+  resources :bookings, only: [:edit, :update]
+
+  resources :users, only: [:show]
   get '/dashboard', to: 'dashboard#index'
 end
