fb80dad364bcc695f4285f31db9e31c52f7ff63b
Ryan Kam <rkam0725@gmail.com>
2021-05-27 18:03:35 +0900

    also changed create workout page

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,13 +1,10 @@
 class Workout < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  # has_many :users, through :bookings
-  CATEGORIES = ['Yoga', 'Crossfit', 'Kick-boxing', 'Swimming', 'Jogging', 'badminton', 'volleyball', 'Basketball', 'Football', 'Fitness', 'Zumba', 'Salsa', 'Weight Training']
 
   CLUBS = ["Team", "Group", "Club"]
 
-  validates :category, :location, :name, presence: true
-  validates :category, inclusion: { in: CATEGORIES }
+  validates :category, :location, :name, :price, presence: true
   has_one_attached :photo
 
   geocoded_by :location
@@ -18,12 +15,12 @@ class Workout < ApplicationRecord
   #     if workout.unique
   # end
 
-    include PgSearch::Model
-          pg_search_scope :search_by_category_and_location,
-            against: [ :category, :location ],
-            using: {
-              tsearch: { prefix: true } # <-- now `superman batm` will return something!
-            }
+  include PgSearch::Model
+  pg_search_scope :search_by_category_and_location,
+    against: [ :category, :location ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 
 
 end
